8ebf10dd7c93da1a41f2e655d2cffa0b0cd4dbb1
Taseer8 <taseer08.xworkz@gmail.com>
2025-03-11 22:16:05 +0530

    locical program

--- Main.java
+++ Main.java
@@ -1,26 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[][] array = {{3, 15, 5}, {4, 5, 6}, {5, 7, 9}};
-        int[][] array1 = {{1, 2, 3}, {4, 6, 5}, {5, 6, 8}};
-        FindMaxElementArray.findTheMaxElementInArray(array);
-        FindMaxElementArray.findTheSumOfEachColunm(array);
-        FindMaxElementArray.findTheSumOfEachArray(2,array1);
-        FindMaxElementArray.findTheMaxValueInTheColumn(1,array);
-        FindMaxElementArray.findRowWithMaxSum(array1);
-        FindMaxElementArray.findTheSumOfSepcifiedRow(0,array);
-        FindMaxElementArray.findMaxElementFromTheEachRow(1,array1);
-        FindMaxElementArray.findtheSumOfSpecifiedColumn(2,array);
+//        int[][] array = {{3, 15, 5}, {4, 5, 6}, {5, 7, 9}};
+//        int[][] array1 = {{1, 2, 3}, {4, 6, 5}, {5, 6, 8}};
+//        FindMaxElementArray.findTheMaxElementInArray(array);
+//        FindMaxElementArray.findTheSumOfEachColunm(array);
+//        FindMaxElementArray.findTheSumOfEachArray(2,array1);
+//        FindMaxElementArray.findTheMaxValueInTheColumn(1,array);
+//        FindMaxElementArray.findRowWithMaxSum(array1);
+//        FindMaxElementArray.findTheSumOfSepcifiedRow(0,array);
+//        FindMaxElementArray.findMaxElementFromTheEachRow(1,array1);
+//        FindMaxElementArray.findtheSumOfSpecifiedColumn(2,array);
 
-        Rotation.rotateArray180Degree(array);
-        Rotation.rotateMatrix90Degree(array1);
+//        Rotation.rotateArray180Degree(array);
+//        Rotation.rotateMatrix90Degree(array1);
 
-        ArthamticOperationsWithArrays.substract2matrix(array1,array);
-        ArthamticOperationsWithArrays.add2Matrix(array,array1);
-        ArthamticOperationsWithArrays.mulitplyThe2Matrix(array,array1);
-
-
+//        ArthamticOperationsWithArrays.substract2matrix(array1,array);
+//        ArthamticOperationsWithArrays.add2Matrix(array,array1);
+//        ArthamticOperationsWithArrays.mulitplyThe2Matrix(array,array1);
+        int array2[] = {1 , 2 ,5, 3 , 4  ,5};
+        int arr[] = {1,2,3,4,5,6};
+        int arr1[] = {1,2,7,5,9,10};
+        int arr3[] = {1,0,3,0,4};
+        int n = array2.length;
+//        SubArray.printTwoParts(array2, n);
+//        SubArray.findSumOfNNaturalNumber(10);
+//        SubArray.findTheIntersectionofElement(arr1,arr);
+        SubArray.findDublicate(array2);
+        SubArray.findTheIndexValueOfParticularValue(array2);
+        SubArray.moveZeroToEnd(arr3);
+        System.out.println(Arrays.toString(arr3));
 
     }
-
 }
