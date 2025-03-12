649296ec09eca49e7adfd0a049a66e82a5df12ab
Taseer8 <taseer08.xworkz@gmail.com>
2025-03-12 15:59:17 +0530

    locical program

--- Main.java
+++ Main.java
@@ -20,18 +20,27 @@ public class Main {
 //        ArthamticOperationsWithArrays.substract2matrix(array1,array);
 //        ArthamticOperationsWithArrays.add2Matrix(array,array1);
 //        ArthamticOperationsWithArrays.mulitplyThe2Matrix(array,array1);
-        int array2[] = {1 , 2 ,5, 3 , 4  ,5};
+        int[] arri = {1,9,9,8};
+       int[] result = ArthamticOperationsWithArrays.add1toarray(arri);
+        System.out.println(Arrays.toString(result));
+        int array2[] = {1,2,5,3,4,5};
         int arr[] = {1,2,3,4,5,6};
-        int arr1[] = {1,2,7,5,9,10};
+        int arr1[] = {1,22,7,5,9,10};
         int arr3[] = {1,0,3,0,4};
         int n = array2.length;
 //        SubArray.printTwoParts(array2, n);
 //        SubArray.findSumOfNNaturalNumber(10);
 //        SubArray.findTheIntersectionofElement(arr1,arr);
-        SubArray.findDublicate(array2);
-        SubArray.findTheIndexValueOfParticularValue(array2);
-        SubArray.moveZeroToEnd(arr3);
-        System.out.println(Arrays.toString(arr3));
+//        SubArray.findDublicate(array2);
+//        SubArray.findTheIndexValueOfParticularValue(array2);
+//        SubArray.moveZeroToEnd(arr3);
+//        System.out.println(Arrays.toString(arr3));
+//
+//        BaseiProgramming.findMaxElement(arr1);
+//        BaseiProgramming.findSecondMaxElement(arr1);
+//        BaseiProgramming.feboninceSeries(10);
+//        BaseiProgramming.factorical(5);
+//        BaseiProgramming.reverseNumber(345);
 
     }
 }
